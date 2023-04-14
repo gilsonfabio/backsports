c163dd3ae05f3c1fcbf10de05840a5f0d911fa98
Gilson Fábio <gilsonfabio@gmail.com>
2023-04-14 14:27:37 -0300

    Atualização da autenticação do JWT

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, HEAD" );
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization, x-access-token");
     app.use(cors());
     next();
 });
--- src/routes.js
+++ src/routes.js
@@ -20,7 +20,7 @@ function verifyJWT(req, res, next){
     //console.log('verificando token...')
     const token = req.headers["x-access-token"];
     if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-
+    
     jwt.verify(token, process.env.SECRET_JWT, (err, userInfo) => {
         if (err) {
            return res.status(403).send({ auth: false, message: 'Token invalid!' });
